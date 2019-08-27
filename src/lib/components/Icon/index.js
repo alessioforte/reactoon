99d22f1362453ed40f868b9cb10dd8e856e19152
Alessio Forte <fortealessio@gmail.com>
2019-08-27 17:52:43 +0200

    change Icon

--- src/lib/components/Icon/index.js
+++ src/lib/components/Icon/index.js
@@ -5,15 +5,17 @@ import icons from './icons'
 const Icon = ({ name, color, size }) => {
     const { d, width } = icons[name]
     return (
-        <Svg
-            color={color}
-            height={size || 30}
-            x='0px'
-            y='0px'
-            viewBox={`0 0 ${width} 512`}
-        >
-            <path d={d} />
-        </Svg>
+        <div>
+            <Svg
+                color={color}
+                height={size || 30}
+                x='0px'
+                y='0px'
+                viewBox={`0 0 ${width} 512`}
+            >
+                <path d={d} />
+            </Svg>
+        </div>
     )
 }
 
