ba95bdd912e39128fcdb5ebac8fde71e06912eb3
Alessio Forte <fortealessio@gmail.com>
2019-09-08 17:15:19 +0200

    transform modules commonjs

--- src/lib/index.js
+++ src/lib/index.js
@@ -1,27 +1,27 @@
 import Button from './components/Button';
-// import Menu from './components/menu/menu'
-import Dropdown from './components/Dropdown';
+import Input from './components/Input';
+import Checkbox from './components/Checkbox';
+import Toggle from './components/Toggle';
+import Radio from './components/Radio';
 import Select from './components/Select/Select';
 import Multiselect from './components/Select/MultiSelect';
+import DatePicker from './components/DatePicker';
+import Searchbar from './components/Searchbar';
+import Icon from './components/Icon';
+import Tooltip from './components/Tooltip';
+import Dropdown from './components/Dropdown';
 import Filterselect from './components/Filters/FilterSelect';
 // import Filterslider from './components/Filters/FilterSlider'
 // import Slider from './components/slider/slider'
 // import SliderRange from './components/slider/slider-range'
 // import SliderStep from './components/slider/slider-step'
-import Toggle from './components/Toggle';
-import Checkbox from './components/Checkbox';
-import Radio from './components/Radio';
+// import Menu from './components/menu/menu'
 // import Loading from './components/loading/loading'
 // import Spinner from './components/spinner/spinner'
 // import Notification from './components/notification'
-import Icon from './components/Icon';
-import Input from './components/Input';
-import Tooltip from './components/Tooltip';
 // import SideBar from './components/sidebar/sidebar'
 // import Modal from './components/modal/modal'
 // import Calendar from './components/calendar/calendar'
-import DatePicker from './components/DatePicker';
-import Searchbar from './components/Searchbar';
 
 import withTooltip from './hoc/withTooltip';
 
@@ -29,29 +29,18 @@ import Theme from './theme';
 
 export {
   Button,
-  // Menu,
-  Dropdown,
+  Input,
+  Checkbox,
+  Toggle,
+  Radio,
   Select,
   Multiselect,
-  Filterselect,
-  // Filterslider,
-  // Slider,
-  // SliderRange,
-  // SliderStep,
-  Toggle,
-  Checkbox,
-  Radio,
-  // Loading,
-  // Spinner,
-  // Notification,
-  Icon,
-  Input,
-  Tooltip,
-  // SideBar,
-  // Modal,
-  // Calendar,
   DatePicker,
   Searchbar,
+  Icon,
+  Tooltip,
+  Dropdown,
+  Filterselect,
   withTooltip,
   Theme
 };
