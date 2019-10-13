d1aef5ac5af5e39c77929ea7b3f4f0a88f8643c3
Alessio Forte <fortealessio@gmail.com>
2019-10-13 13:56:46 +0200

    dev Message

--- src/lib/components/Message/index.tsx
+++ src/lib/components/Message/index.tsx
@@ -1,7 +1,7 @@
 import React, { FC } from 'react'
 import PropTypes from 'prop-types';
 import styled, { ThemeProvider, withTheme } from 'styled-components'
-import { styles, getColorLuminance } from '../../theme';
+import { styles, getColorLuminance, getContrastYIQ } from '../../theme';
 
 type Props = {
   status: string,
@@ -20,7 +20,7 @@ const Message: FC<Props> = ({ status, children, theme }) => {
 }
 
 Message.propTypes = {
-  status: PropTypes.string,
+  status: PropTypes.string.isRequired,
 }
 
 Message.defaultProps = {
@@ -34,4 +34,5 @@ const Box = styled.div<{status: string}>`
   border-radius: ${props => props.theme.border.radius}px;
   border: 1px solid ${props => props.theme.colors[props.status]};
   background: ${props => getColorLuminance(props.theme.colors[props.status], 0.3)};
+  color: ${props => props.theme.colors[getContrastYIQ(props.theme.colors[props.status])]};
 `
